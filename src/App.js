0bdcfb26d5d80694b42bbf7a788ad81f13025acf
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-11-05 18:51:39 -0500

    moved components into home.js component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
-import WorldInfoContainer from './WorldInfoContainer';
-import Header from './Header';
+import Home from './Pages/Home';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+
 
 function App() {
 
   return (
     <Container>
-      <Header
-        title='COVID-19 Statistics'
-        subTitle='By world, country, and region'
-        imageSource=''
-      />
-      <Row>
-        <WorldInfoContainer />
-      </Row>
+      <Home />
     </Container>
   );
 };
